b00838448f21fa6b1dc9b38b0b0a742d68370ddb
Junior <junior.uniseb8@gmail.com>
2021-12-26 20:57:40 -0300

    Initial API Heroes

---
+++ src/main/java/me/dio/heroesapi/heroesapi/HeroesapiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.heroesapi.heroesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/heroesapi/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.heroesapi.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
